46c9cc0bcf1cde17add7312e7373d2d68062df69
choijinny42 <99052200+choijinny42@users.noreply.github.com>
2022-02-22 14:31:18 -0800

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,29 @@
+package com.jinnychoi.module1_1;
+
+public class Triangle extends TwoDShape {
+    double side1;
+    double side2;
+    double side3;
+
+    public Triangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    private double heronsHeight() {
+       double s = (side1 + side2 + side3) / 2;
+       double area = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+       return 2 * (area / side2);
+
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+}
